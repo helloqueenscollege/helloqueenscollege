1395386f61bd15b31a0663251a93d8018fdf4b69
Christian Sakai <christianmsakai@gmail.com>
2016-03-24 14:30:09 -0400

    finish contributors feature

--- js/contributors.js
+++ js/contributors.js
@@ -1,5 +1,6 @@
 var contributors = (function(window) {
 
+  // API Fetching
   var repoUrl = 'https://api.github.com/repos/helloqueenscollege/helloqueenscollege';
   var contributorsUrl = repoUrl + '/contributors';
 
@@ -26,7 +27,14 @@ var contributors = (function(window) {
           };
         });
 
-        console.log(forksCount, contributors);
+        return renderContentHeader(forksCount, contributors.length) +
+               renderContributorTitle() +
+               renderGallery(contributors);
+      })
+      .then(function(renderedString) {
+        var domNode = window.document.getElementById('contributors');
+
+        domNode.innerHTML = renderedString;
       })
       .catch(handleError);
   };
@@ -40,7 +48,77 @@ var contributors = (function(window) {
     console.log('Something is wrong, ', err);
   }
 
+  // Rendering
+  var renderHeadingIcon = function(type, count) {
+    var openHeadingTag = "<div class='heading'>";
+
+    var renderedHeading = "<h2 class='icon'><span class='fa fa-" +
+                          type + " fa-1x'></span> " +
+                          count + "</h2>";
+
+    var closeHeadingTag = "</div>";
+
+    return openHeadingTag + renderedHeading + closeHeadingTag;
+  };
+
+  var renderHeading = function(sentence) {
+    var openHeadingTag = "<div class='heading'>";
+    var renderedHeading = "<h2>" + sentence + "</h2>";
+    var closeHeadingTag = "</div>";
+
+    return openHeadingTag + renderedHeading + closeHeadingTag;
+  };
+
+  var renderContentHeader = function(forks, pullRequests) {
+    var openContentTag = "<div class='content flex-container'>";
+    var sentence = forks > pullRequests ? "Need more PRs!" : "Nice PRs!"; 
+    var closeContentTag = "</div>";
+    
+    return openContentTag + 
+           renderHeadingIcon('users', forks) +
+           renderHeadingIcon('code-fork', pullRequests) +
+           renderHeading(sentence) +
+           closeContentTag;
+  };
+  
+  var renderContributor = function(contributor) {
+    var openContributorTag = "<div class='contributor'>";
+    var closeContributorTag = "</div>";
+    var contributorAvatar = "<img class='avatar' src='" + 
+                        contributor.avatar_url + "' alt='" +
+                        contributor.login + "'>";
+    var contributorHandle = "<h3 class='handle'><a href='" + 
+                            contributor.html_url + "'>@" +
+                            contributor.login + 
+                            "</a></h3>";
+
+    return openContributorTag + 
+           contributorAvatar +
+           contributorHandle +
+           closeContributorTag;
+  };
+
+  var renderContributorTitle = function() {
+    return "<h1 class='text-center'>Contributors</h1>";
+  };
+
+  var renderGallery = function(contributors) {
+    var openGalleryTag = "<div class='gallery'>";
+    var closeGalleryTag = "</div>";
+    var renderedContributors = contributors.sort(function(contribA, contribB) {
+      return contribA.contributions < contribB.contributions;
+    }).map(function(contributor) {
+      return renderContributor(contributor);
+    }).join('');
+
+
+    return openGalleryTag +
+           renderedContributors + 
+           closeGalleryTag;
+  };
+
   return {
     init: init
   };
+
 })(window);
